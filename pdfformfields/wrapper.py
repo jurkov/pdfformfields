c1cc4c186335f4dfd5e1707035614c7e14acc23c
Long <Nguyen.Ba.Long13@gmail.com>
2019-09-29 22:19:56 +0200

    Cleaned up code

--- pdfformfields/wrapper.py
+++ pdfformfields/wrapper.py
@@ -1,3 +1,10 @@
+""" pdfformfields
+
+Python wrapper around pdftk to fill in pdf form fields.
+See https://github.com/Balonger/pdfformfields
+
+"""
+
 import platform
 import subprocess
 from typing import Dict
@@ -6,13 +13,32 @@ from xfdfgen import Xfdf
 
 from wrapper_runtime_tests import *
 
-if platform.system() == 'Windows':
-    probable_pdftk_location = os.path.join(r"C:\Program Files (x86)\PDFtk Server\bin", "pdftk.exe")
-    if os.path.isfile(probable_pdftk_location):
-        pdftk_path = probable_pdftk_location
+
+def get_pdftk_path():
+    """
+    Tries to infer the bash command for pdftk.
+
+    Returns: Bash command for pdftk
+
+    """
+    # TODO add mac support
+    # TODO test on linux
+
+    if platform.system() == 'Windows':
+        probable_pdftk_location = os.path.join(r"C:\Program Files (x86)\PDFtk Server\bin\pdftk.exe")
+        if os.path.isfile(probable_pdftk_location):
+            return probable_pdftk_location
+
+    if platform.system() == 'Linux':
+        probable_pdftk_location = '/usr/bin/pdftk'
+        if os.path.isfile(probable_pdftk_location):
+            return probable_pdftk_location
+
+    return "pdftk"
 
 
-def fill_form_fields(input_pdf: str, form_field_dictionary: Dict[str, str], output_pdf: str, flatten: bool = False):
+def fill_form_fields(input_pdf: str, form_field_dictionary: Dict[str, str], output_pdf: str, flatten: bool = False,
+                     pdftk_command: str = None):
     """
     Takes an input pdf containing form fields, and fills them in from a python dictionary.
 
@@ -22,33 +48,41 @@ def fill_form_fields(input_pdf: str, form_field_dictionary: Dict[str, str], outp
                                                 and fill in values as values
         output_pdf (str): path to the output pdf
         flatten (Optional: bool): if set to true the output pdf won't be editable
+        pdftk_command (Optional: str): if the function can't find the pdftk executable, you can set it manually here.
     """
-    fill_form_fields_sanity_checks(input_pdf, form_field_dictionary, output_pdf)
+    # Try to infer the location of pdftk
+    if pdftk_command is None:
+        pdftk_command = get_pdftk_path()
 
-    filename_of_input_pdf = os.path.basename(input_pdf)
+    # Sanity checks
+    fill_form_fields_sanity_checks(input_pdf, form_field_dictionary, output_pdf, pdftk_command)
 
     # Create an xfdf file from the form_field directory and write it to a temporary file
     xfdf = Xfdf(input_pdf, form_field_dictionary)
+    filename_of_input_pdf = os.path.basename(input_pdf)
     temp_xfdf_path = f"{filename_of_input_pdf}.xfdf"
     xfdf.write_xfdf(temp_xfdf_path)
 
     # Bash command to fill in the input_pdf using pdftk
-    cmd = [pdftk_path, input_pdf, "fill_form", temp_xfdf_path, "output", output_pdf]
+    cmd = [pdftk_command, input_pdf, "fill_form", temp_xfdf_path, "output", output_pdf]
     if flatten:
         cmd.append("flatten")
-    subprocess.run(cmd)
+
+    try:
+        subprocess.run(cmd)
+    except (FileNotFoundError, OSError):
+        # Remove temporary xfdf file
+        if os.path.exists(temp_xfdf_path):
+            os.remove(temp_xfdf_path)
+
+        # bash could not execute pdftk_command.
+        err_msg = f"{pdftk_command} could not be executed in bash." \
+                  f" See bash error at https://github.com/Balonger/pdfformfields"
+        raise OSError(err_msg)
 
     # Remove temporary xfdf file
     if os.path.exists(temp_xfdf_path):
         os.remove(temp_xfdf_path)
 
 
-example_dict = {
-    "first_name": "john",
-    "last_name": "doe"
-}
 
-example_pdf = os.path.join(r"F:\Github_projects\pdfformfields\example\example_pdf.pdf")
-output_pdf = os.path.join(r"F:\Github_projects\pdfformfields\example\output2.pdf")
-
-fill_form_fields(example_pdf, example_dict, output_pdf, flatten=True)
--- pdfformfields/wrapper_runtime_tests.py
+++ pdfformfields/wrapper_runtime_tests.py
@@ -3,15 +3,20 @@ import os
 from typing import Dict
 
 
-def fill_form_fields_sanity_checks(input_pdf: str, form_field_dictionary: Dict[str, str], output_pdf: str):
-    # if not os.path.isfile(input_pdf):
-    #     raise OSError(f"{input_pdf} does not exist.")
+def fill_form_fields_sanity_checks(input_pdf: str, form_field_dictionary: Dict[str, str], output_pdf: str,
+                                   pdftk_command: str):
+
+    # input_pdf checks #################################################################################################
+    if not os.path.isfile(input_pdf):
+        raise OSError(f"{input_pdf} does not exist.")
 
     if not input_pdf.endswith(".pdf"):
         raise ValueError(f"{input_pdf} is not a pdf file.")
 
+    # form_field_dictionary checks #####################################################################################
     if not isinstance(form_field_dictionary, dict):
         raise TypeError("form_field_dictionary must be a dictionary")
 
+    # output_pdf checks ################################################################################################
     if not output_pdf.endswith(".pdf"):
         raise ValueError(f"{output_pdf} is not a pdf file.")
